b050d631b8ebf7f0c11d9ee6a853374378efe2a0
user <user@DESKTOP-PJ74UJ4>
2019-01-21 14:59:20 +0900

    메인화면 일단 구현 완료..

--- miniProject/src/miniProject/MainScreen.java
+++ miniProject/src/miniProject/MainScreen.java
@@ -1,6 +1,7 @@
 package miniProject;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 
@@ -8,6 +9,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
@@ -16,38 +18,43 @@ import javax.swing.border.LineBorder;
 public class MainScreen extends JFrame{
 	// 메인 이미지(타이틀과 별개)
 	private Image mainimage = Toolkit.getDefaultToolkit().createImage("resource/images/title/mainimage.png");
+	private Image titleimage = Toolkit.getDefaultToolkit().createImage("resource/images/title/titleimage.jpg");
 	private Image iconimage = Toolkit.getDefaultToolkit().createImage("resource/images/title/titleicon.png");
 	private JButton btnGamestart, btnGameQuit;
 	
-	public static void main(String[] args) {
-		System.out.println("[info] 프로그램 실행");
-		new MainScreen().createMainScreen();
-	}
+	public MainScreen() {  
+	    super("Dragon Slayer");
+	    setSize(700, 525);
+	    setResizable(false);
+	    
+	    JLayeredPane layer = getLayeredPane();
+	    
+	    JLabel mainimageLabel = new JLabel(new ImageIcon(mainimage));
+	    mainimageLabel.setBounds(0, 0, 700, 525);
+	    
+	    JLabel titleimageLabel = new JLabel(new ImageIcon(titleimage));
+	    titleimageLabel.setBounds(0, 0, 302, 182);
+	    
+	    JPanel buttonPanel = new JPanel(null);
+	    buttonPanel.setBounds(400, 200, 120, 240);
+	    buttonPanel.setOpaque(false);
+	    
+	    btnGamestart = new JButton("게임시작");
+	    btnGamestart.setBounds(5, 10, 100, 50);
+	    
+	    btnGameQuit = new JButton("게임종료");
+	    btnGameQuit.setBounds(5, 70, 100, 50);
+	    
+	    buttonPanel.add(btnGamestart);
+	    buttonPanel.add(btnGameQuit);
+	    
+	    layer.add(mainimageLabel, new Integer(1));
+	    layer.add(titleimageLabel, new Integer(2));
+	    layer.add(buttonPanel, new Integer(3));
+	    setVisible(true);
+	  } 
 	
-	public void createMainScreen() {
-		setSize(700, 525);
-		setTitle("Dragon Slayer");
-		setIconImage(iconimage);
-		setLayout(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// 메인 이미지
-		JLabel mainLabel = new JLabel();
-		mainLabel.setBounds(0, 0, 700, 525);
-		mainLabel.setIcon(new ImageIcon(mainimage));
-		
-		JLabel titleLabel = new JLabel();
-		titleLabel.setBounds(500, 100, 150, 80);
-		titleLabel.setBorder(new LineBorder(Color.BLACK));
-		
-		JPanel testpanel = new JPanel(null);
-		testpanel.setBounds(50, 50, 200, 200);
-		testpanel.setBorder(new LineBorder(Color.RED));
-		
-		add(mainLabel);
-		add(testpanel);
-		setVisible(true);
-	}
-	
-	
+	  public static void main(String[] args) {  
+		  new MainScreen();
+	  }  
 }
